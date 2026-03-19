fa1c6ca18476f3d9500d4847bc9369d95801d352
saikiran3080 <132368576+saikiran3080@users.noreply.github.com>
2026-03-20 00:09:44 +0530

    update

---
+++ src/main/java/cicd/SampleProjectForCICD/SampleProjectForCicdApplication.java
@@ -0,0 +1,13 @@
+package cicd.SampleProjectForCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectForCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectForCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cicd/SampleProjectForCICD/SampleProjectForCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package cicd.SampleProjectForCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectForCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
